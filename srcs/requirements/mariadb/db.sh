18524febd99c8480e7d48ed970dd5fc7bec513b5
ewoillar <ewoillar@ewoillar.ewoillar.42.fr>
2025-02-28 17:59:49 +0100

    time for mariadb to not working

---
+++ srcs/requirements/mariadb/db.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+service mariadb start;
+mariadb -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+mariadb -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+mariadb -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+mariadb -e "FLUSH PRIVILEGES;"
+mariadbd-admin -u root -p$SQL_ROOT_PASSWORD shutdown
+exec mariadb-safe
