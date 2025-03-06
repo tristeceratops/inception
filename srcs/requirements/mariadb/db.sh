2b2987233ee740cafe257d4d227aa0ea5a007a73
ewoillar <ewoillar@ewoillar.ewoillar.42.fr>
2025-03-06 18:04:11 +0100

    new error message

--- srcs/requirements/mariadb/db.sh
+++ srcs/requirements/mariadb/db.sh
@@ -4,7 +4,7 @@ service mariadb start;
 mariadb -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 mariadb -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
 mariadb -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+#mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 mariadb -e "FLUSH PRIVILEGES;"
-mariadbd-admin -u root -p$SQL_ROOT_PASSWORD shutdown
-exec mariadb-safe
+mariadb-admin -u root -p$SQL_ROOT_PASSWORD shutdown
+exec mariadbd-safe
