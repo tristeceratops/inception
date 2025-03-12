7df6c9d304d0ee0fe0405e17778aaea7b3236db2
ewoillar <ewoillar@ewoillar.ewoillar.42.fr>
2025-03-12 17:25:56 +0100

    marche po

--- srcs/requirements/mariadb/db.sh
+++ srcs/requirements/mariadb/db.sh
@@ -1,10 +1,21 @@
 #!/bin/sh
+#export SQL_DATABASE="website"
+#export SQL_USER="ewoillar"
+#export SQL_PASSWORD="1234"
+#export SQL_ROOT_PASSWORD="r1234"
+#export ADMIN_USER="ewoillar"
+#export ADMIN_PASSWORD="adm1234"
+#export ADMIN_EMAIL="ewoillar@student.42luxembourg.lu"
+#export USER_LOGIN="pibernar"
+#export USER_EMAIL="test@student.42luxembourg.lu"
+
+#echo "SQL_ROOT_PASSWORD: $SQL_ROOT_PASSWORD"
 
 service mariadb start;
 mariadb -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 mariadb -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-mariadb -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-#mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-mariadb -e "FLUSH PRIVILEGES;"
+mariadb -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';" -u root
+mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+mariadb -e "FLUSH PRIVILEGES;" -u root --password=$SQL_ROOT_PASSWORD
 mariadb-admin -u root -p$SQL_ROOT_PASSWORD shutdown
 exec mariadbd-safe
--- srcs/requirements/wordpress/auto_config.sh
+++ srcs/requirements/wordpress/auto_config.sh
@@ -1,22 +1,4 @@
 #!/bin/sh
 
-sleep 10
-
-# Créer la configuration WordPress
-wp config create \
-    --dbname=$SQL_DATABASE \
-    --dbuser=$SQL_USER \
-    --dbpass=$SQL_PASSWORD \
-    --dbhost=mariadb:3306 --path='/var/www/wordpress/' --allow-root
-
-# Installer le coeur de WordPress
-wp core install \
-    --title="SummeryLeak" \
-    --admin_user=$ADMIN_USER \
-    --admin_password=$ADMIN_PASSWORD \
-    --admin_email=$ADMIN_EMAIL \
-    --skip-email --path='/var/www/wordpress/' --allow-root
-
-# Créer un utilisateur WordPress
-wp user create $USER_LOGIN $USER_EMAIL --path='/var/www/wordpress/' --allow-root
-
+ls /var/www/wordpress/
+cat /var/www/wordpress/index.php
