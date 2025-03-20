06f4a79632a475b78b0e2d51f36b2d2dab3142d0
ewoillar <ewoillar@ewoillar.ewoillar.42.fr>
2025-03-20 14:49:02 +0100

    wweeeehheee

--- srcs/requirements/mariadb/db.sh
+++ srcs/requirements/mariadb/db.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
-service mariadb start;
-mariadb -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-mariadb -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-mariadb -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';" -u root
-mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-mariadb -e "FLUSH PRIVILEGES;" -u root --password=$SQL_ROOT_PASSWORD
-mariadb-admin -u root -p$SQL_ROOT_PASSWORD shutdown
-exec mariadbd-safe
+mysqld_safe --user=mysql &
+sleep 5
+
+if [ ! -f "/var/lib/mysql/.initialized" ]; then
+    echo "initializing"
+    mysql -u root -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+    mysql -u root -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+    mysql -u root -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+    mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+    mysql -u root -p"${SQL_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"
+    touch /var/lib/mysql/.initialized
+fi
+
+mysqladmin -u root -p"${SQL_ROOT_PASSWORD}" shutdown
+
+exec mysqld_safe --user=mysql
